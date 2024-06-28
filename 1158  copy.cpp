431862a88f5edfb7bea2ebcb2549e8dad6b7391d
Hun-X1 <younghun4218@gmail.com>
2024-06-28 15:45:36 +0900

    야매 링크드리스트 풀이 성공, 이게 더 빠르네 ㅋㅋ

--- 1158  copy.cpp
+++ 1158  copy.cpp
@@ -6,23 +6,25 @@ int main(void){
     cin.tie(0);
     int N, K;
     cin>>N>>K;
-    list<int> L;    
-    for(int i=1; i<=N; i++){
-        L.push_back(i);    
+    int pre[5001], nxt[5001];
+       
+    for(int i=1; i<=N; ++i){
+        pre[i]=i-1;
+        nxt[i]=i+1;
     }
-    list<int>::iterator iter=L.begin();
-    cout<<"<";
-    while(N--){
-        for(int i=1; i<K; i++){
-                iter++;
-            if(iter==L.end()){            
-                iter=L.begin();
-            }
-        }  
-        cout<<*iter;
+     pre[1]=N;
+     nxt[N]=1;     
+     int iter=1;
+     cout<<"<";
+     while(N--){
+        for(int i=1;i<K;++i){
+            iter=nxt[iter];
+        }
+        cout<<iter;
+        pre[nxt[iter]]=pre[iter];
+        nxt[pre[iter]]=nxt[iter];
+        iter=nxt[iter];
         if(N>0) cout<<", ";
-        iter=L.erase(iter);
-        if(iter==L.end()) iter=L.begin();          
-    }
-    cout<<">";
+     }
+     cout<<">"; 
 }
