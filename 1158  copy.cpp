8a67c95b6fee5bb7c2fac87e70ab53befbb90303
Hun-X1 <younghun4218@gmail.com>
2024-06-28 15:24:40 +0900

    야매 linkedlsit로 풀어보자

---
+++ 1158  copy.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+//custom linked list ver
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N, K;
+    cin>>N>>K;
+    list<int> L;    
+    for(int i=1; i<=N; i++){
+        L.push_back(i);    
+    }
+    list<int>::iterator iter=L.begin();
+    cout<<"<";
+    while(N--){
+        for(int i=1; i<K; i++){
+                iter++;
+            if(iter==L.end()){            
+                iter=L.begin();
+            }
+        }  
+        cout<<*iter;
+        if(N>0) cout<<", ";
+        iter=L.erase(iter);
+        if(iter==L.end()) iter=L.begin();          
+    }
+    cout<<">";
+}
