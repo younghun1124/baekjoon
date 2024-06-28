a99700e1743732b982702de97cf825f072bcb418
Hun-X1 <younghun4218@gmail.com>
2024-06-28 17:20:25 +0900

    아직푸는중

---
+++ 1874.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N;
+    cin>>N;
+    stack<int> S;
+    string ans;
+    for(int i=1; i<=N; ++i){
+        int k;        
+        cin>>k;
+        while(k>i) {
+            S.push(i);
+            
+        }
+        if(k<i) {
+            ans
+        }
+    }
+}
