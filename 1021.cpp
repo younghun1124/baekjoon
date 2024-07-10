83bc7fb876d5c4937236ed63fa3afd399ba2d82d
Hun-X1 <younghun4218@gmail.com>
2024-07-10 16:26:00 +0900

    first sol, 근데 야매로 푼거같은 느낌

--- 1021.cpp
+++ 1021.cpp
@@ -1,19 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 deque<int> dq;
-void move_left(){
+int tempcnt=0;
+void move_L(){
     dq.push_back(dq.front());
     dq.pop_front();
 }
-void move_right(){
+void move_R(){
     dq.push_front(dq.back());
     dq.pop_back();
+}
+void move_until_find(int input){
+    tempcnt=0;
+    while(!(dq.front()==input)){
+        move_R();   
+        tempcnt++;
+    }
+    
 }
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
    
-    int N, M;
+    int N, M, ans=0;
+
     cin>>N>>M;
-    
+    while(N--){
+        dq.push_front(N+1);
+    }
+    while(M--){
+        int input;
+        cin>>input;
+        move_until_find(input);
+        if(tempcnt<=dq.size()/2){
+            ans+=tempcnt;
+        }else{
+            ans+=(dq.size()-tempcnt);
+        }
+        dq.pop_front();
+    }
+    cout<<ans;
 }
