f29e5fcf3be9a8dca77a0436c4a816c977baac5d
Hun-X1 <younghun4218@gmail.com>
2024-07-27 11:59:22 +0900

    원래 내 로직

--- 1926.cpp
+++ 1926.cpp
@@ -4,40 +4,44 @@
 using namespace std;
     int board[502][502];
     bool vis[502][502];
-       int n,m;
-      int dx[4]={1,0,-1,0};
-    int dy[4]={0,1,0,-1};
 int main(void){
     ios::sync_with_stdio(0);
-    cin.tie(0); 
+    cin.tie(0);
+    int n,m;
     cin>>n>>m;
-    for(int i=0; i<n;i++)
-        for(int j=0; j<m;j++)
-            cin>>board[i][j];  
+    int dx[4]={1,0,-1,0};
+    int dy[4]={0,1,0,-1};
+
+    for(int i=0; i<n;i++){
+        for(int j=0; j<m;j++){
+            cin>>board[i][j];
+        }
+    }
+    queue<pair<int,int>> Q;
     int picture_num=0;
     int max_area=0;
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
-            if(vis[i][j]||board[i][j]==0) continue;
-            picture_num++;
-            queue<pair<int,int>> Q;
-            vis[i][j]=1;
-            Q.push({i,j});
-            int tmp_area=0;
-            while(!Q.empty()){
-                tmp_area++;
-                pair<int, int> cur=Q.front(); Q.pop();
-                for(int dir=0; dir<4; dir++){
-                    int nx=cur.X+dx[dir];
-                    int ny=cur.Y+dy[dir];
-                    if(nx<0||nx>=n||ny<0||ny>=m) continue;
-                    if(vis[nx][ny]||board[nx][ny]!=1) continue;
-                    vis[nx][ny]=1;
-                    Q.push({nx,ny});
+            if(!vis[i][j]&&board[i][j]){
+                picture_num++;
+                vis[i][j]=1;
+                int tmp_area=1;
+                Q.push({i,j});
+                while(!Q.empty()){
+                    pair<int, int> cur=Q.front(); Q.pop();
+                    for(int dir=0; dir<4; ++dir){
+                        int nx=cur.X+dx[dir];
+                        int ny=cur.Y+dy[dir];
+                        if(nx<0||nx>=n||ny<0||ny>=m) continue;
+                        if(vis[nx][ny]||board[nx][ny]!=1) continue;
+                        vis[nx][ny]=1;
+                        tmp_area++;
+                        Q.push({nx,ny});
+                    }
                 }
+                if(tmp_area>max_area) max_area=tmp_area;
             }
-            max_area=max(max_area,tmp_area);            
         }
     }
-    std::cout<<picture_num<<'\n'<<max_area;
+    cout<<picture_num<<"\n"<<max_area;
 }
