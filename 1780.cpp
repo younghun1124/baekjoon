849f4251134167cefb98f74187ed9b66b9ab7e8b
YoungHun <younghun4218@gmail.com>
2024-08-03 13:49:44 +0900

    refector

--- 1780.cpp
+++ 1780.cpp
@@ -2,25 +2,27 @@
 using namespace std;
 int arr[3000][3000];
 int ans[3];
-void func(int n, int idx, int idx2){
-    bool valid=true;
+
+bool check(int n, int idx, int idx2){
     int tmp=arr[idx][idx2];
     for(int i=0; i<n; ++i)
-        for(int j=0; j<n; ++j)
-            if(tmp!=arr[i+idx][j+idx2]){
-                valid=false;
-                break;
-            }
-    if(!valid){
-        for(int i=0; i<3; ++i)
+            for(int j=0; j<n; ++j)
+                if(tmp!=arr[i+idx][j+idx2])
+                    return false;
+                    return true;
+    
+}
+
+void func(int n, int idx, int idx2){
+    if(check(n,idx,idx2)){
+        ans[arr[idx][idx2]+1]++;
+        return
+    }
+    for(int i=0; i<3; ++i)
             for(int j=0; j<3; ++j)
              func(n/3, i*n/3+idx, j*n/3+idx2);
         return;
-    }
-    if(tmp==-1) ans[0]++;
-    else if(tmp==0) ans[1]++;
-    else ans[2]++;
-    return;
+    
 }
 int main(void){
     ios::sync_with_stdio(0);
