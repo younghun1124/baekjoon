8b0c3145999772b4d2f4ba6b99617b80be785272
YoungHun <younghun4218@gmail.com>
2024-09-10 20:24:56 +0900

    1

--- 2448_unsol.cpp
+++ 2448_unsol.cpp
@@ -1,9 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-char board[5000][5000];
+char board[4000][8000];
 
 void solve(int n, int x, int y){
-
+    if(n==1){
+        board[x][y]='*';
+        return;
+    }
+    n=n/2;
+    
+    
 }
 
 int main(void){
@@ -13,5 +19,5 @@ int main(void){
     cin>>n;
     for(int i=0; i<n; ++i)
     fill_n(board[i],n,' ');
-
+    
 }
