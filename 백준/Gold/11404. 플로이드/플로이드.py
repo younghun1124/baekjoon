105a6b4a3e371f591d0a03fdfcd08ea9e2fac63a
younghun1124 <83543030+younghun1124@users.noreply.github.com>
2024-11-20 12:55:32 +0900

    [Gold IV] Title: 플로이드, Time: 172 ms, Memory: 111692 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,44 +1,23 @@
 import sys
-input = sys.stdin.read
-INF = float('inf')
+input=sys.stdin.readline
+print=sys.stdout.write
+n=int(input())
+m=int(input())
+D=[[float("inf")]*n for _ in range(n)]
+for i in range(n):
+    D[i][i]=0
+for _ in range(m):
+    a,b,c= list(map(int,input().split()))
+    D[a-1][b-1]=min(D[a-1][b-1],c) #시작도시와 도착 도시를 연결하는 노선은 하나가 아닐 수 있으므로 최소값 사용
 
-def floyd_warshall(n, bus_info):
-    # 초기 비용 테이블 생성
-    dist = [[INF] * n for _ in range(n)]
-    
-    # 자기 자신으로 가는 비용은 0으로 설정
+for k in range(n):
     for i in range(n):
-        dist[i][i] = 0
-    
-    # 버스 정보를 이용해 초기 거리 설정
-    for a, b, c in bus_info:
-        a -= 1  # 도시 번호를 0부터 시작하도록 변경
-        b -= 1
-        dist[a][b] = min(dist[a][b], c)  # 같은 경로 여러 개일 수 있으므로 최소값 사용
-    
-    # 플로이드-워셜 알고리즘 실행
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
-    
-    # 결과 테이블 처리
-    for i in range(n):
+       
         for j in range(n):
-            if dist[i][j] == INF:  # 경로가 없는 경우
-                dist[i][j] = 0
+           
+            D[i][j]=min(D[i][j],D[i][k]+D[k][j])
+result = []
+for row in D:
+    result.append(" ".join(str(0 if i == float("inf") else i) for i in row))
+sys.stdout.write("\n".join(result) + "\n")
     
-    return dist
-
-# 입력 받기
-data = input().splitlines()
-n = int(data[0])
-m = int(data[1])
-bus_info = [tuple(map(int, line.split())) for line in data[2:]]
-
-# 플로이드-워셜 수행
-result = floyd_warshall(n, bus_info)
-
-# 결과 출력
-for row in result:
-    print(' '.join(map(str, row)))
