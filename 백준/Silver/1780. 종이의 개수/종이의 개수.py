9e34fc0a48c3716239f12dc632a4983f62b2a4ec
younghun1124 <83543030+younghun1124@users.noreply.github.com>
2024-12-21 23:16:10 +0900

    [Silver II] Title: 종이의 개수, Time: 4004 ms, Memory: 71328 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.py
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -3,26 +3,21 @@ input=stdin.readline
 n=int(input())
 board=[list(map(int,input().split())) for _ in range(n)]
 ans=[0,0,0]
-def check(board):
-    z=board[0][0]
-    for row in board:
-        for e in row:
+def paper_check(r,c,n):
+    z=board[r][c]
+    for row in board[r:r+n]:
+        for e in row[c:c+n]:
             if z!=e:
-                return 2
-    return z
-def cut(r,c,board):
-    size=len(board)//3
-    newboard=[row[c*size:(c+1)*size] for row in board[r*size:(r+1)*size]]
-    return newboard
-def paper(board): #받은 종이에서 같은 숫자로 채워진 종이를 찾아내는 함수    
-    z=check(board)
-    if z!=2:#종이가 하나로 이루어져 있으면
-        ans[z+1]=ans[z+1]+1 #종이 개수 늘리기(z가 -1 일수 있으니까 인덱스 1씩 더하기)
+                return False
+    return True
+def paper(r,c,n): #받은 종이에서 같은 숫자로 채워진 종이를 찾아내는 함수        
+    if paper_check(r,c,n):#종이가 하나로 이루어져 있으면
+        ans[board[r][c]+1]=ans[board[r][c]+1]+1  #종이 개수 늘리기(z가 -1 일수 있으니까 인덱스 1씩 더하기)
         return    
-    for r in range(3):
-        for c in range(3):
-            paper(cut(r,c,board))
-paper(board)
+    for rstep in range(3):
+        for cstep in range(3):
+            paper(r+rstep*(n//3),c+cstep*(n//3),n//3)
+           
+paper(0,0,n)
 for i in ans:
-    print(i)
-                
+    print(i)                
