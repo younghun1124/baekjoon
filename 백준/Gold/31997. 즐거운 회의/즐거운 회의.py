59b11024578df2172e498d87099736090e24e7d8
younghun1124 <83543030+younghun1124@users.noreply.github.com>
2025-02-03 11:42:02 +0900

    [Gold V] Title: 즐거운 회의, Time: 780 ms, Memory: 136376 KB -BaekjoonHub

--- 백준/Gold/31997. 즐거운 회의/즐거운 회의.py
+++ 백준/Gold/31997. 즐거운 회의/즐거운 회의.py
@@ -1,30 +1,23 @@
 import sys
-input = sys.stdin.readline
-output = sys.stdout.write
+input=sys.stdin.read
+output=sys.stdout.write
+data=input().splitlines()
+n,m,t=map(int,data[0].split())
+time=[tuple(map(int,x.split())) for x in data[1:1+n]]
+friend={}
+for row in data[1+n:]:
+    c,d=map(int,row.split())
+    friend.setdefault(c,[]).append(d)
 
-n, f, T = map(int, input().split())
-t = [tuple(map(int, input().split())) for _ in range(n)]
-friend = {}
+ans=[0]*(t+1) #딱 t 까지 회의가 있으니까 t+1 이어야 함.
+for i in friend:
+    for j in friend[i]:
+        start= max(time[i-1][0],time[j-1][0])
+        end= min(time[i-1][1],time[j-1][1])
+        if start<end:
+            ans[start]+=1
+            ans[end]-=1
+for i in range(1,t+1):
+    ans[i]+=ans[i-1]
 
-for _ in range(f):
-    c, d = map(int, input().split())
-    friend.setdefault(c, []).append(d)
-
-ans = [0] * (T + 1)  # 차이 배열을 위한 크기 (T+1)
-
-# 차이 배열을 이용한 갱신
-for i in range(1, n + 1):
-    if i in friend:
-        for j in friend[i]:
-            start = max(t[i - 1][0], t[j - 1][0])
-            end = min(t[i - 1][1], t[j - 1][1])
-            if start < end:  # 유효한 시간 구간만 갱신
-                ans[start] += 1
-                ans[end] -= 1  # end에서 감소
-
-# 누적 합 계산 (Prefix Sum)
-for k in range(1, T):
-    ans[k] += ans[k - 1]
-
-# 출력
-output("\n".join(map(str, ans[:T])) + "\n")
+output('\n'.join(map(str,ans[:t])))
