5e49f7d18792c87d525fc11956d9a05c97b10e00
younghun1124 <83543030+younghun1124@users.noreply.github.com>
2025-02-04 11:50:38 +0900

    [Silver V] Title: 볶음밥 지키기, Time: 1848 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Silver/30891. 볶음밥 지키기/볶음밥 지키기.py
@@ -0,0 +1,25 @@
+import math
+N,R=map(int,input().split())
+bap=[tuple(map(int,input().split())) for _ in range(N)]
+def is_in_wok(W,B):
+    X,Y=W
+    bx,by=B
+    if math.sqrt((X-bx)**2+(Y-by)**2)<=R:
+        return True
+    else:
+        return False
+
+ans=0
+count=0
+for X in range(-100,101):
+    for Y in range(-100,101):
+        temp=0
+        for B in bap:
+            if is_in_wok((X,Y),B):
+                temp+=1
+            if count<temp:
+                count=temp
+                ans=(X,Y)
+print(ans[0],ans[1])
+            
+    
