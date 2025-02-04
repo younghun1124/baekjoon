04c2beb950ff56253d01b94cfe1223b05fa5d776
younghun1124 <83543030+younghun1124@users.noreply.github.com>
2025-02-04 12:08:28 +0900

    [Silver V] Title: 볶음밥 지키기, Time: 1020 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/30891. 볶음밥 지키기/볶음밥 지키기.py
+++ 백준/Silver/30891. 볶음밥 지키기/볶음밥 지키기.py
@@ -1,26 +1,18 @@
 import sys
-input=sys.stdin.readline
-N,R=map(int,input().split())
-bap=[tuple(map(int,input().split())) for _ in range(N)]
-def is_in_wok(W,B):
-    X,Y=W
-    bx,by=B
-    if (X-bx)**2+(Y-by)**2<=R**2:
-        return True
-    else:
-        return False
+input = sys.stdin.readline
+N, R = map(int, input().split())
+bap = [list(map(int, input().split())) for _ in range(N)]  # 튜플 → 리스트
 
-ans=0
-count=0
-for X in range(-100,101):
-    for Y in range(-100,101):
-        temp=0
-        for B in bap:
-            if is_in_wok((X,Y),B):
-                temp+=1
-        if count<temp:
-            count=temp
-            ans=(X,Y)
-print(*ans)
-            
-    
+def is_in_wok(x, y, bx, by):
+    return (x - bx) ** 2 + (y - by) ** 2 <= R ** 2  # 튜플 언패킹 제거
+
+ans = 0
+count = 0
+for X in range(-100, 100):  # 101 → 100
+    for Y in range(-100, 100):
+        temp = sum(1 for bx, by in bap if is_in_wok(X, Y, bx, by))  # 리스트 컴프리헨션 사용
+        if count < temp:
+            count = temp
+            ans = (X, Y)
+
+print(ans[0], ans[1])
