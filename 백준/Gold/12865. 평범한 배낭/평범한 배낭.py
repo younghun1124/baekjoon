192dff0cd5811752b23c1e1fbf27260a4b6abd5c
younghun1124 <83543030+younghun1124@users.noreply.github.com>
2025-02-05 16:03:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 3976 ms, Memory: 281576 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,15 +1,13 @@
-import sys
-input=sys.stdin.readline
 n,k=map(int,input().split())
-d=[[0]*(k+1) for _ in range(n+1)]
-item=[(0,0)]
-item.extend([tuple(map(int,input().split())) for _ in range(n)]) #무게, 가치
 
-for i in range(1,n+1): #i는 아이템 인덱스
-    for bagW in range(1,k+1):
-        W,V=item[i]
-        if W<=bagW: #가방에 넣을 수 있다.
-            d[i][bagW]=max(d[i-1][bagW],V+d[i-1][bagW-W])
-        if W>bagW: #가방에 넣지못한다
-            d[i][bagW]=d[i-1][bagW]
+items=[list(map(int,input().split())) for _ in range(n)] #W, V
+d=[[0]*(k+1) for _ in range(n+1)] #0 인덱스 아이템
+
+for i in range(1,n+1): 
+    W,V=items[i-1]
+    for j in range(1,k+1):
+        if W>j: #담을수 없다
+            d[i][j]=d[i-1][j] #이전 무게 그대로 유지
+        else:
+            d[i][j]=max(d[i-1][j-W]+V,d[i-1][j]) #빼고 새로 담는것과 안담는것 사이에 더 큰값
 print(d[n][k])
