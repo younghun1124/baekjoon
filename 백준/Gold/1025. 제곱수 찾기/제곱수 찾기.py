da7312318572e3aa8928ba5534326d4b621fbe7f
younghun1124 <83543030+younghun1124@users.noreply.github.com>
2025-02-06 13:04:43 +0900

    [Gold V] Title: 제곱수 찾기, Time: 64 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Gold/1025. 제곱수 찾기/제곱수 찾기.py
+++ 백준/Gold/1025. 제곱수 찾기/제곱수 찾기.py
@@ -1,27 +1,23 @@
 import math
 
-n, m = map(int, input().split())
-board = [list(map(int, input())) for _ in range(n)]
+n,m=map(int,input().split())
+board=[list(map(int,input())) for _ in range(n)]
 
-ans = -1
-
-# 가능한 모든 step 크기 조합 탐색 (-n ~ n, -m ~ m)
-for stepx in range(-n, n):
-    for stepy in range(-m, m):
-        if stepx == 0 and stepy == 0:
+ans=-1
+for stepx in range(-n,n+1):
+    for stepy in range(-m,m+1):
+        if stepx==0 and stepy==0:
             continue
-        # 시작점 탐색
-        for x in range(n):
-            for y in range(m):
-                tempx, tempy = x, y
-                num = 0
-                # 범위를 벗어나지 않는 동안 숫자 생성
-                while 0 <= tempx < n and 0 <= tempy < m:
-                    num = num * 10 + board[tempx][tempy]
-                    # 완전 제곱수 판별
-                    if int(math.sqrt(num))**2 == num:
-                        ans = max(ans, num)
-                    tempx += stepx
-                    tempy += stepy
-
+        for x in range(0,n): #x시작점
+            for y in range(0,m): #y시작점
+                tempx=x
+                tempy=y
+                temp=0
+                while 0<=tempx<n and 0<=tempy<m:
+                    temp=temp*10+board[tempx][tempy]
+                    if int(math.sqrt(temp))**2 == temp:
+                        ans=max(ans,temp)
+                    tempx+=stepx
+                    tempy+=stepy
+                    
 print(ans)
