78ef62e462847ae17f5e96cfe303f10dd103e2ea
younghun1124 <83543030+younghun1124@users.noreply.github.com>
2025-02-11 18:57:15 +0900

    [Silver III] Title: 게임, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.py
+++ 백준/Silver/1072. 게임/게임.py
@@ -1,12 +1,11 @@
 import math
 X,Y=map(int,input().split())
 Z=(Y*100)//X
-# print(Z)
 K=Z+1
 if K>=100:
     print(-1)
     exit()
 
-i=((K*X)-(100*Y))//(100-K)
-i=i if ((K*X)-(100*Y))%(100-K)==0 else i+1
+i=math.ceil((K*X-100*Y)/(100-K))
+
 print(i)
