4b6ae0cee3f1da9a55220a161a6993bc93d1439c
younghun1124 <83543030+younghun1124@users.noreply.github.com>
2025-02-18 17:29:54 +0900

    [Gold V] Title: LCS, Time: 428 ms, Memory: 57628 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,11 +1,10 @@
-s1=input()
-s2=input()
-
-D=[[0]*(len(s2)+1) for _ in range(len(s1)+1)]
-for i in range(1,len(s1)+1):
-    for j in range(1,len(s2)+1):
-        if s1[i-1]==s2[j-1]:
+A=input()
+B=input()
+D=[[0]*(len(B)+1) for _ in range((len(A)+1))]
+for i in range(1,len(A)+1):
+    for j in range(1,len(B)+1):
+        if A[i-1]==B[j-1]:
             D[i][j]=D[i-1][j-1]+1
         else:
             D[i][j]=max(D[i-1][j],D[i][j-1])
-print(D[-1][-1])
+print(D[len(A)][len(B)])
