28a98bd73b2910ef4c19a569c5c5aa4f037d71d4
younghun1124 <83543030+younghun1124@users.noreply.github.com>
2025-02-27 11:08:35 +0900

    [Gold V] Title: 퇴사 2, Time: 3164 ms, Memory: 348712 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,14 +1,12 @@
 import sys
 input=sys.stdin.readline
-N=int(input().strip())
+N=int(input())
 item=[0]+[list(map(int,input().split())) for _ in range(N)]
-
-D=[0]*(N+1) #D[i] i일의 수익 최대값
-for i in range(1,N+1): #i일째 상담스케쥴 고려하기
+D=[0]*(N+1)
+for i in range(1,N+1):
+    D[i]=max(D[i-1],D[i])
     t,p=item[i]
-    finsh=i+t-1
-    if finsh<=N:#상담 완료 날짜가 N 안쪽이면 계산
-        D[finsh]=max(D[finsh],D[i-1]+p)
-    D[i]=max(D[i],D[i-1]) #그리고 i일째 최대값은 전날꺼 그대로 하기vs 
-
+    end=t+i-1
+    if end<=N:
+        D[end]=max(D[end],D[i-1]+p)
 print(D[N])
