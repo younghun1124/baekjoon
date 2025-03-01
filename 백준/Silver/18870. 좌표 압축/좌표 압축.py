382e94ad7b788f4308e2f24ff8b7a46921619d96
younghun1124 <83543030+younghun1124@users.noreply.github.com>
2025-03-02 08:30:47 +0900

    [Silver II] Title: 좌표 압축, Time: 6456 ms, Memory: 194480 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,16 +1,18 @@
+import sys
+output=sys.stdout.write
 N=int(input())
 X=list(map(int,input().split()))
 sorted_X=sorted(list(set(X)))
 D={}
-
+ans=[0]*N
 for i in range(N):#X`i 찾기
     x=X[i]
     s=0
     e=len(sorted_X)-1
     if D.get(x,-1)!=-1:
-        print(D[x],end=' ')
+        ans[i]=D[x]
         continue
-    ans=0
+    
     while s<=e: # m번째 수(sorted_X)가 xi 보다 큰지 작은지 확인 
         m=(s+e)//2
         if x<sorted_X[m]: #넘친다
@@ -18,6 +20,6 @@ for i in range(N):#X`i 찾기
             
         else:
             s=m+1
-            ans=m
-    D[x]=ans
-    print(ans,end=' ')
+            ans[i]=m
+    D[x]=ans[i]
+output(' '.join(map(str,ans)))
