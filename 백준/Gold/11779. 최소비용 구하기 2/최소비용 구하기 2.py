90dabc70eb73f4f6f096f8377f8dc6c0d212e072
younghun1124 <83543030+younghun1124@users.noreply.github.com>
2025-03-01 09:48:37 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 264 ms, Memory: 58232 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -1,41 +1,39 @@
 import sys
-from heapq import heappop,heappush
+from heapq import heappop, heappush
 input=sys.stdin.readline
-n=int(input().strip())
-m=int(input().strip())
-graph=[list() for _ in range(n+1)]
+n=int(input())
+m=int(input())
+INF=int(1e9)
+G=[list() for _ in range(n+1)] 
+D=[INF]*(n+1)
+pre=[0]*(n+1)
 for _ in range(m):
     a,b,c=map(int,input().split())
-    graph[a].append((c,b))
-hist=[0]*(n+1)
-INF=int(1e18)
-D=[INF]*(n+1)
-def dijkstra(start):    
-    q=[]    
+    G[a].append((c,b))
+start,end=map(int,input().split())
+def dijkstra(start):
     D[start]=0
+    q=[]
     heappush(q,(0,start))
-    
     while q:
         w,node=heappop(q)
         if w>D[node]:
             continue
-        for n_w, n_node in graph[node]:
+        for n_w,n_node in G[node]:
             cost=n_w+D[node]
             if cost<D[n_node]:
-                heappush(q,(cost,n_node))
                 D[n_node]=cost
-                hist[n_node]=node #어떤 노드에서 도착했는지 쓰기
-start,end=map(int,input().split())
-
+                heappush(q,(cost,n_node))
+                pre[n_node]=node
 dijkstra(start)
 print(D[end])
-
-cur=end
-ans=[end]
+i=end
+way=[]
 while True:
-    cur=hist[cur]
-    ans.insert(0,cur)
-    if cur==start:
+    way.append(i)
+    if i==start:
         break
-print(len(ans))
-print(' '.join(map(str,ans)))
+    i=pre[i]
+print(len(way))
+way.reverse()
+print(' '.join(map(str,way)))
