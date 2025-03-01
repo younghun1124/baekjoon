4658b77af3d4fdaaa90c8e83b5dffa3dcfc4d75e
younghun1124 <83543030+younghun1124@users.noreply.github.com>
2025-03-01 13:42:19 +0900

    [Gold III] Title: 텀 프로젝트, Time: 2296 ms, Memory: 48476 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -1,30 +1,28 @@
 import sys
-sys.setrecursionlimit(10**6) 
-T = int(input())
+sys.stdin.readline
+T=int(input())
+
+def DFS(start):
+    global team
+    temp=[]
+    node=start
+    while True:
+        visit[node]=True
+        temp.append(node)
+        node=board[node]
+        if visit[node]:
+            if node in temp:
+                team+=len(temp)-temp.index(node)
+            return
+        
 for _ in range(T):
-    N = int(input())
-    selected = [0] + list(map(int, input().split()))
+    n=int(input())
+    board=[0]+list(map(int,input().split()))
+    visit=[False]*(n+1)
+    team=0
 
-    visited = [False] * (N+1)
-    team_mems = 0
+    for start in range(1,n+1):
+        if not visit[start]:
+            DFS(start)
 
-    def dfs(i):
-        global team_mems
-
-        visited[i] = True # 나부터
-        team.append(i) # 팀 후보에 추가
-        select = selected[i] # 다음 사람 지목
-
-        if visited[select]: # 지목한 사람을 이미 방문했을 경우
-            if select in team: # 그 사람이 팀 후보에 있다면
-            	# 그 사람부터 이후에 팀에 들어온 사람들은 한 팀이 된다
-                team_mems += len(team[team.index(select):])
-        else: # 아니라면 지목 받은 사람이 다른 사람 지목
-            dfs(select)
-
-    for i in range(1, N+1):
-        if not visited[i]:
-            team = []
-            dfs(i)
-
-    print(N - team_mems)
+    print(n-team)
