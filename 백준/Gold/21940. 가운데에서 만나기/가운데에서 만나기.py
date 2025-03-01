07cfbe0314fff26619dc5451dec4e94c4816baac
younghun1124 <83543030+younghun1124@users.noreply.github.com>
2025-03-01 12:20:56 +0900

    [Gold IV] Title: 가운데에서 만나기, Time: 1844 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/21940. 가운데에서 만나기/가운데에서 만나기.py
@@ -0,0 +1,35 @@
+import sys
+sys.stdin.readline
+N,M=map(int,input().split())
+INF=int(1e9)
+G=[[INF]*(N+1) for _ in range(N+1)]
+for _ in range(M):
+    a,b,t=map(int,input().split())
+    G[a][b]=min(t,G[a][b])
+K=int(input())
+C=list(map(int,input().split()))
+for i in range(N+1):
+    G[i][i]=0
+def floyd():
+    for k in range(1,N+1):
+        for i in range(1,N+1):
+            for j in range(1,N+1):
+                G[i][j]=min(G[i][k]+G[k][j],G[i][j])
+                
+floyd()
+ans=[]
+mindist=INF
+for x in range(1,N+1):
+    dist=0
+    for f in C:
+        if dist>=INF:
+            temp=[INF]
+            break
+        dist=max(G[f][x]+G[x][f],dist)
+    if dist<mindist:
+        ans=[x]
+        mindist=dist
+    elif dist==mindist:
+        ans.append(x)
+        
+print(' '.join(map(str,ans)))
