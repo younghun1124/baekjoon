6c18a6c45a41a9f6b4771ce6b64319862d90a617
younghun1124 <83543030+younghun1124@users.noreply.github.com>
2025-03-02 18:37:02 +0900

    [Gold IV] Title: 운동, Time: 5588 ms, Memory: 44100 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.py
+++ 백준/Gold/1956. 운동/운동.py
@@ -1,26 +1,32 @@
 import sys
-input=sys.stdin.readline
-V,E=map(int,input().split())
-INF=int(1e7)
-G=[[INF]*(V+1) for _ in range(V+1)]
-for _ in range(E):
-    a,b,c=map(int,input().split())
-    G[a][b]=c
+input = sys.stdin.readline
+
+#floyd
+INF = int(1e9)
+n, m = map(int, input().split())
+graph = [[INF]*(n+1) for _ in range(n+1)]
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+def floyd(graph, n):
+    dist = [row[:] for row in graph]
     
-for i in range(V+1):
-    G[i][i]=0
+    for k in range(1, n+1):
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                if dist[i][j] > dist[i][k] + dist[k][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+    
+    return dist
 
-for k in range(1,V+1):
-    for i in range(1,V+1):
-        for j in range(1,V+1):
-            G[i][j]=min(G[i][j],G[i][k]+G[k][j])
-ans=INF
-
-for i in range(1,V+1):
-    for j in range(1,V+1):
-        if i==j:
-            continue
-        ans=min(G[i][j]+G[j][i],ans)
-if ans==INF:
+dist = floyd(graph, n)
+ans = []
+for i in range(1, n+1):
+    ans.append(dist[i][i])
+min_dist = min(ans)
+if min_dist >= INF:
     print(-1)
-else : print(ans)
+else:
+    print(min_dist)
